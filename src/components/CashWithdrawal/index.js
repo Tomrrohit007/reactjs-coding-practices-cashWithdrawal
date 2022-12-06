fe31fe0e8971d5646c825a36b5bddbf3fb9c8ff6
Rohit Tomar <tomarrohit5034@gmail.com>
2022-12-06 14:37:46 +0000

    solution

---
+++ src/components/CashWithdrawal/index.js
@@ -0,0 +1,41 @@
+import {Component} from 'react'
+import DenominationItem from '../DenominationItem/index.css'
+import './index.css'
+
+class CashWithdrawal extends Component {
+  render() {
+    const {denominationsList} = this.props
+
+    return (
+      <div className="main-container">
+        <div className="card">
+          <div className="user-details">
+            <div className="name-container">
+              <p className="name-icon">S</p>
+            </div>
+
+            <h1 className="username">Sarah Williams</h1>
+          </div>
+          <div className="balance-details">
+            <h1 className="your-balance">Your Balance</h1>
+            <div>
+              <h1 className="balance">2000</h1>
+              <p className="text">In Rupees</p>
+            </div>
+          </div>
+          <div className="withdrawal-section">
+            <h1 className="withdrawal">Withdraw</h1>
+            <p className="choose-amt">Choose sum (IN RUPEES) </p>
+            <ul className="withdrawal-amt">
+              {denominationsList.map(eachValue => (
+                <DenominationItem eachValue={eachValue} key={eachValue.id} />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CashWithdrawal
---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const DenominationItem = props => {
+  const {eachValue} = props
+  const {id, value} = eachValue
+  return (
+    <button className="button" type="button">
+      Hello
+    </button>
+  )
+}
+
+export default DenominationItem
